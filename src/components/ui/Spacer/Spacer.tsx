3d716a8445670f10ac686ec6b00f70d685a95c42
Kaito-Dogi <kendobu0405@gmail.com>
2023-11-08 04:19:57 +0900

    feat: Spacer を実装

---
+++ src/components/ui/Spacer/Spacer.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+import { View } from "react-native";
+
+import { styles } from "./Spacer.styles";
+
+/** @package */
+export const Spacer: FC = () => {
+  return <View style={styles.container} />;
+};
