c1b7785c7b9c817d1a6feef77f226da7a56b98ad
Kaito-Dogi <kendobu0405@gmail.com>
2023-11-11 02:55:09 +0900

    feat: HomeScreen の実装

--- src/app/events/index.tsx
+++ src/app/events/index.tsx
@@ -1,29 +1,5 @@
-import { Link } from "expo-router";
-
-import { Text } from "@/components/ui/Text";
 import { HomeScreen } from "@/screens/HomeScreen";
 
 export default function Page() {
-  return (
-    <>
-      <Text text="Events Page" />
-      <Link
-        href={{
-          pathname: "events/[id]",
-          params: { id: 1 },
-        }}
-      >
-        詳細ページ1へ
-      </Link>
-      <Link
-        href={{
-          pathname: "events/[id]",
-          params: { id: 2 },
-        }}
-      >
-        詳細ページ2へ
-      </Link>
-      <HomeScreen />
-    </>
-  );
+  return <HomeScreen />;
 }
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,35 +1,46 @@
+import { router } from "expo-router";
 import { FC, useCallback } from "react";
-import { View } from "react-native";
+import { FlatList, View } from "react-native";
 
 import { EventListItem } from "@/components/model/event/EventListItem";
+import { Spacer } from "@/components/ui/Spacer";
 import { Text } from "@/components/ui/Text";
 import { mockEvents } from "@/mocks/events";
 import { Event } from "@/models/Event";
+import { dimens } from "@/styles/dimens";
 
 import { styles } from "./HomeScreen.styles";
 
 /** @package */
 export const HomeScreen: FC = () => {
   const onEventClick = useCallback((event: Event) => {
-    console.log("event: ", event.name);
+    router.push(`/events/${event.id}`);
   }, []);
+
   return (
     <View style={styles.container}>
-      <Text
-        text={"おすすめのイベント"}
-        size="xl"
-        fontWeight="bold"
-        numberOfLines={1}
+      <FlatList<Event>
+        data={mockEvents}
+        renderItem={({ item }) => (
+          <EventListItem event={item} onEventClick={onEventClick} />
+        )}
+        keyExtractor={(item) => `${item.id}`}
+        ListHeaderComponent={
+          <>
+            <Text
+              text={"おすすめのイベント"}
+              size="xl"
+              fontWeight="bold"
+              numberOfLines={1}
+            />
+            <Spacer height={"m"} />
+          </>
+        }
+        ItemSeparatorComponent={() => <Spacer height={"m"} />}
+        contentContainerStyle={{
+          paddingVertical: dimens.xl,
+        }}
       />
-      {mockEvents.map((event) => {
-        return (
-          <EventListItem
-            event={event}
-            onEventClick={onEventClick}
-            key={event.id}
-          />
-        );
-      })}
     </View>
   );
 };
