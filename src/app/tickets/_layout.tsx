42553af1eeb96af867b6a01a36360098b25baf3b
Kaito-Dogi <kendobu0405@gmail.com>
2023-11-13 03:45:42 +0900

    fix: Android でモーダルを実装するため、JS Stack を実装

--- src/app/tickets/_layout.tsx
+++ src/app/tickets/_layout.tsx
@@ -1,5 +1,37 @@
-import { Stack } from "expo-router";
+import { ParamListBase, StackNavigationState } from "@react-navigation/native";
+import {
+  createStackNavigator,
+  StackNavigationEventMap,
+  StackNavigationOptions,
+  TransitionPresets,
+} from "@react-navigation/stack";
+import { withLayoutContext } from "expo-router";
+
+const { Navigator } = createStackNavigator();
+
+const JsStack = withLayoutContext<
+  StackNavigationOptions,
+  typeof Navigator,
+  StackNavigationState<ParamListBase>,
+  StackNavigationEventMap
+>(Navigator);
 
 export default function Layout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <JsStack screenOptions={{ headerShown: false }}>
+      <JsStack.Screen name="index" />
+      {/**
+       * モーダルは Android で動作しないため JS Stack Navigator で独自に実装する必要がある
+       * https://github.com/expo/router/issues/640
+       * https://docs.expo.dev/router/advanced/stack/#javascript-stack-with-react-navigationstack
+       */}
+      <JsStack.Screen
+        name="[id]"
+        options={{
+          ...TransitionPresets.ModalPresentationIOS,
+          presentation: "modal",
+        }}
+      />
+    </JsStack>
+  );
 }
